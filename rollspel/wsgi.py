19478b1ef65ba26371c8fd3a013f3fd3eb093855
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-03-08 19:58:26 +0000

    Första commiten, boilerplate

---
+++ rollspel/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rollspel project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rollspel.settings')
+
+application = get_wsgi_application()
